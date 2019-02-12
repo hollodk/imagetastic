05eee5f56abfdf2975c05ac737d58a913462393b
Michael Holm <hollo@hollo.dk>
2019-02-12 20:50:13 +0100

    Fixed test

--- tests/DefaultTest.php
+++ tests/DefaultTest.php
@@ -8,7 +8,7 @@ final class DefaultTest extends TestCase
 {
     public function testTypos()
     {
-        $imagetastic = new \Imagetastic\Client();
+        $imagetastic = new \Imagetastic\Client('key', 'project');
 
         // first test, just to make sure we dont have any typos
         $this->assertEquals(1, 1);
